c93ace22e943eba0e3a648bcda500323ca51f2cb
Irina <online.info2579@gmail.com>
2021-10-31 00:34:38 -0400

    tracking lovejava

---
+++ src/HWClass01/ILoveJava.java
@@ -0,0 +1,13 @@
+package HWClass01;
+
+public class ILoveJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Iam a Java Programmer.");
+System.out.println("I study at Syntax.");
+System.out.println("I Love Java!");
+System.out.println("!!!");
+	}
+
+}
